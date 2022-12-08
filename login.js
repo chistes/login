339bcbc14ccc7ea00e74e192bdac4066c5c11b5e
chistes <120127623+chistes@users.noreply.github.com>
2022-12-08 09:50:51 -0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,40 @@
+let log ="RRIA";
+let password = "HELLO";
+let condicion1;
+let condicion2;
+
+let userlog = prompt(" Ingrese su Usuario")
+do{
+
+    if(userlog !== log) 
+    {
+        userlog = prompt("Usuario Incorrecto")
+    }
+
+    else
+{
+    condicion1 = userlog
+}
+
+}
+
+
+
+while (condicion1 !==  userlog)
+
+let UserPassW = prompt("Ingrese su Contraseña")
+
+do{
+
+    if(UserPassW !== password)
+    {
+        UserPassW =promt("Contraseña Incorrecta")
+    }
+
+    else
+    {
+        condicion2 == UserPassW
+    }
+}
+
+while (condicion2 !== UserPassW)
